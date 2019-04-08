5874d2082dcf16276fb435f69b9448e32846580c
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-04-09 00:40:20 +0200

    Create Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>Movie database</h1>
+                <Navbar />
             </div>
         );
     }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="nav-wrapper container">
+                <a href="#" className="brand-logo">
+                    Movie database
+                </a>
+            </div>
+        </nav>
+    );
+};
+export default Navbar;
