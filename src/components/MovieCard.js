473b46b4504d2f4166ddfe9572d17a220967a83e
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-04-10 12:08:27 +0200

    Create MovieCard component

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,34 @@
 import React from "react";
 
-const MovieCard = () => {
-    return null;
+const Movie = props => {
+    return (
+        <div className="col s12 m6 l3">
+            <div className="card">
+                <div className="card-image">
+                    {props.image == true ? (
+                        <img
+                            className=""
+                            src={`https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg`}
+                            alt="n"
+                            style={{ width: "100%", height: 360 }}
+                        />
+                    ) : (
+                        <img
+                            className=""
+                            src={`http://image.tmdb.org/t/p/w185${props.image}`}
+                            alt="n"
+                            style={{ width: "100%", height: 360 }}
+                        />
+                    )}
+                </div>
+                <div class="card-content">
+                    <p>
+                        <a href="#">View Details</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
-export default MovieCard;
+export default Movie;
